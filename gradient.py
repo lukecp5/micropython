cd829988841b074da5512156a094e575e57b9ead
Luke Poirrier <lukepoirrier5@gmail.com>
2023-02-05 01:58:55 -0500

    Refactor code

--- gradient.py
+++ gradient.py
@@ -3,20 +3,18 @@
 #>> Imports
 import machine, neopixel
 
-#>> Board Config
+#>> LED strip configuration
 n = 18 ## Number of LEDS on strip
 p = 14 ## GPIO Pin number
-
-#>> Create a NeoPixel object(np)
 np = neopixel.NeoPixel(machine.Pin(p), n) 
 
 #>> Functions
-## Sets all LEDs on strip to (0,0,0) color to clear set colors
-def clear():
+def clear(): # Sets all LEDs on strip to (0,0,0) color to clear set colors
   for i in range(n):
     np[i] = (0, 0, 0)
     np.write()
-    
+
+#>> Main 
 np[0] = (255, 0, 0)
 np[2] = (125, 204, 223)
 np[4] = (120, 153, 23)
