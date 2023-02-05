285a7210352876d27b47cfd7524a9d9d21c1f88a
Luke Poirrier <lukepoirrier5@gmail.com>
2023-02-05 02:14:57 -0500

    Add functionality to generate a color wheel on circular LEDs

--- gradient.py
+++ gradient.py
@@ -18,6 +18,19 @@ def cycle(r, g, b, wait):
     np.write()
     time.sleep_ms(wait)
 
+# ## function to go through all colors 
+# def wheel(pos):
+#   # Input a value 0 to 255 to get a color value.
+#   # The colours are a transition r - g - b - back to r.
+#   if pos < 0 or pos > 255:
+#     return (0, 0, 0)
+#   if pos < 85:
+#     return (255 - pos * 3, pos * 3, 0)
+#   if pos < 170:
+#     pos -= 85
+#     return (0, 255 - pos * 3, pos * 3)
+#   pos -= 170
+#   return (pos * 3, 0, 255 - pos * 3)
 
 ## rainbow 
 def rainbow_cycle(wait):
